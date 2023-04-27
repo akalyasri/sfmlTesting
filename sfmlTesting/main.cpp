b5c4931a4ed1da985d75eadf4f020b50330c8275
Akalya Sridharan <akalya.s.sri@gmail.com>
2023-04-27 16:55:15 -0700

    creating menu

--- sfmlTesting/main.cpp
+++ sfmlTesting/main.cpp
@@ -4,12 +4,10 @@ int main()
 {
 
 
-	srand((unsigned int)time(NULL));
-	game g1;
-	g1.runGame();
 
 
 
+	/*
 
 
 	//sf::RenderWindow window(sf::VideoMode(880 * 2, 1200), "Game Board!");
@@ -36,9 +34,87 @@ int main()
 
 	//}
 
+	*/
+
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Battleship Menu");
+
+    // load the background image
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("Textures/sea.jpg")) {
+        std::cout << "Error loading background texture." << std::endl;
+        return 1;
+    }
+    sf::Sprite background(backgroundTexture);
+
+    // load the font
+    sf::Font font;
+    if (!font.loadFromFile("Fonts/ARIAL.TTF")) {
+        std::cout << "Error loading font." << std::endl;
+        return 1;
+    }
+
+    // create the title text
+    sf::Text title("BATTLESHIP", font, 50);
+    title.setPosition(200, 50);
+
+    // create the "Play Game" text
+    sf::Text playGame("Play Game", font, 30);
+    playGame.setPosition(300, 200);
+
+    // create the "Rules" text
+    sf::Text rules("Rules", font, 30);
+    rules.setPosition(340, 250);
+
+    // create the "Exit" text
+    sf::Text exit("Exit", font, 30);
+    exit.setPosition(350, 300);
+
+    // run the main loop
+    while (window.isOpen()) {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    if (playGame.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                        // play game option selected
+                        window.close();
+                        // TODO: launch game screen
+
+                        srand((unsigned int)time(NULL));
+                        game g1;
+                        g1.runGame();
 
 
+                    }
+                    else if (rules.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                        // rules option selected
+                        window.close();
+                        // TODO: launch rules screen
+                    }
+                    else if (exit.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                        // exit option selected
+                        window.close();
+                    }
+                }
+            }
+        }
 
+        // draw the background, title, and options
+        window.clear();
+        window.draw(background);
+        window.draw(title);
+        window.draw(playGame);
+        window.draw(rules);
+        window.draw(exit);
+        window.display();
+    }
+
+    return 0;
 
 
 	return 0;
