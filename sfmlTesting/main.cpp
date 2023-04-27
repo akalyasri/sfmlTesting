7e54a04e70449a5b2015d1ef503f5a7d1f823518
Harish_Sridharan <harish.s.sri@gmail.com>
2023-04-27 16:24:48 -0700

    minor bug fix - got rid of unnecessary window

--- sfmlTesting/main.cpp
+++ sfmlTesting/main.cpp
@@ -3,34 +3,39 @@
 int main()
 {
 
-	//sf::RenderWindow window(sf::VideoMode(880 * 2, 1200), "Game Board!");
-
-	game g1;
-
-	while (gameWindow.isOpen()) {
-
-		sf::Event event;
-
-		while (gameWindow.pollEvent(event)) {
-
-
-			if (event.type == sf::Event::Closed)
-				gameWindow.close();
-
-		}
-
-
-		gameWindow.clear();
-
-		g1.runGame();
-		// draw here
-
-		gameWindow.display();
-
-
-	}
 
 	srand((unsigned int)time(NULL));
+	game g1;
+	g1.runGame();
+
+
+
+
+
+	//sf::RenderWindow window(sf::VideoMode(880 * 2, 1200), "Game Board!");
+	//while (gameWindow.isOpen()) {
+
+	//	sf::Event event;
+
+	//	while (gameWindow.pollEvent(event)) {
+
+
+	//		if (event.type == sf::Event::Closed)
+	//			gameWindow.close();
+
+	//	}
+
+
+	//	gameWindow.clear();
+
+	//	
+	//	// draw here
+
+	//	gameWindow.display();
+
+
+	//}
+
 
 
 
