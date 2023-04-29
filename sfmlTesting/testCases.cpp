ca4457b9aa5b72c068f91caa18874f6d19b4ab25
Akalya Sridharan <akalya.s.sri@gmail.com>
2023-04-28 21:04:30 -0700

    created SFMLwindow test case

--- sfmlTesting/testCases.cpp
+++ sfmlTesting/testCases.cpp
@@ -20,10 +20,41 @@ void testBombPlacents(void) {
 void testSFMLwindow(void) {
 
 
+	sf::RenderWindow testWindow(sf::VideoMode(800, 800), "SFML Window");
+
+    sf::Font font;
+
+    if (!font.loadFromFile("Fonts/arialbd.ttf")) {
+
+        std::cout << "Error loading font." << std::endl;
+        return;
+    }
+
+    sf::Text text("SFML window is working !!", font, 50);
+    text.setPosition(200, 49);
+
+    while (testWindow.isOpen()) {
+
+        sf::Event event;
+        while (testWindow.pollEvent(event)) {
+           
+
+            if (event.type == sf::Event::Closed) {
+
+                testWindow.close();
+            }
+        }
+
+        
+        testWindow.draw(text);
+        testWindow.display();
+    }
 
 }
 
 void testMouseInput(void) {
+
+
 
 
 
